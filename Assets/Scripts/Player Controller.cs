8ba96a1bab07f76b9bfd8100e75aba0050efa8dc
vezamm <149106775+vezamm@users.noreply.github.com>
2024-10-22 18:49:53 +0100

    Movement
    cinemachine
    new input system
    character controller
    the character is not grounded at all time it must be fixed

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.AssemblyQualifiedNameParser;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float Playerspeed = 1.0f;
+    [SerializeField] private float playerSprintspeed = 2.0f;
+    [SerializeField]
+    private float jumpheight = 1.0f;
+    [SerializeField]
+    private float gravityvalue = -9.81f;
+    [SerializeField]
+    private InputActionReference movementControl;
+    [SerializeField]
+    private float rotationspeed = 4f;
+    //private InputActionReference sprintControl;
+    [SerializeField]
+    private InputActionReference jumpControl;
+    [SerializeField]
+    private bool Grounded;
+    private CharacterController characterController;
+    private Vector3 playerVelocity;
+    private PlayerControle _playercontrole;
+    private Transform cameraMaintransform;
+    private void Awake()
+    {
+        _playercontrole=new PlayerControle();
+    }
+    private void OnEnable()
+    {
+        movementControl.action.Enable();
+         jumpControl.action.Enable();
+        //sprintControl.action.Enable();
+        _playercontrole.Enable();
+    }
+    private void OnDisable()
+    {
+        movementControl.action.Disable();
+        jumpControl.action.Disable();
+        //sprintControl.action.Disable();
+        _playercontrole.Disable();
+    }
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        characterController =gameObject.GetComponent<CharacterController>();
+        cameraMaintransform=Camera.main.transform;
+    }
+
+    void Update()
+    {
+        Grounded = characterController.isGrounded;
+        if (Grounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+        }
+        //Vector2 movement =movementControl.action.ReadValue<Vector2>();
+        Vector2 movement = _playercontrole.Land.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movement.x, 0,movement.y) ;
+
+        move = cameraMaintransform.forward * move.z + cameraMaintransform.right * move.x;
+        move.y = 0;
+
+        Debug.Log(move);
+
+        if (/*jumpControl.action.triggered*/ _playercontrole.Land.Jump.triggered && Grounded)
+        {
+            playerVelocity.y=Mathf.Sqrt(jumpheight*-3.0f*gravityvalue);
+        }
+
+        playerVelocity.y += gravityvalue * Time.deltaTime;
+        characterController.Move(playerVelocity*Time.deltaTime);
+        //int moveValue = /*movementControl.action.ReadValue<int>()*/_playercontrole.Land.Sprinting.;
+
+        if ( /* sprintControl.action.IsPressed()*/_playercontrole.Land.Sprinting.IsPressed())
+        {
+            characterController.Move(move * Time.deltaTime * playerSprintspeed);
+        }
+        else
+           characterController.Move(move*Time.deltaTime*Playerspeed);
+        if(movement!=Vector2.zero)
+        {
+            float targetangle=Mathf.Atan2(movement.x,movement.y)*Mathf.Rad2Deg+cameraMaintransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f,targetangle,0f);
+            transform.rotation=Quaternion.Lerp(transform.rotation,rotation,Time.deltaTime*rotationspeed);
+        }
+        
+    }
+}
