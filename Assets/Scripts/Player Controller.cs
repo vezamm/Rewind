9e440a85f59fa2850bb51a9200afeadce1e5881c
Aziz <azizbouziri10@gmail.com>
2024-11-15 18:25:49 +0100

    Squashed commit of the following:
    commit 711eca7f607d0da612b46b20de162430810aca7e
    Author: Aziz <azizbouziri10@gmail.com>
    Date:   Fri Nov 15 18:23:43 2024 +0100
    
        Jump
    
    commit 28130fdb91a4dc767c68f97c2e05fb0e499e9d46
    Author: Aziz <azizbouziri10@gmail.com>
    Date:   Fri Nov 15 14:19:30 2024 +0100
    
        Squashed commit of the following:
    
        commit 7c6227a6253067ce877daef1689f8bb96656677e
        Author: vezamm <zdsellami@gmail.com>
        Date:   Tue Nov 12 03:11:55 2024 +0100
    
            New ability
    
            +universal RP
            +new cam
            +post-process volume
            +toggle the ability on and off
    
        commit 8032d15844b06aa5f49e6ff37c4af52ad1c4602e
        Author: vezamm <149106775+vezamm@users.noreply.github.com>
        Date:   Wed Oct 23 18:01:17 2024 +0100
    
            shooting
    
        commit dd6eda92022e77531ae6d2d5d090dcc8a8f5385f
        Author: vezamm <149106775+vezamm@users.noreply.github.com>
        Date:   Wed Oct 23 17:26:32 2024 +0100
    
            TMP AND SHOOTING
    
            shooting is kinda wonky working on making it better
    
        commit 8ba96a1bab07f76b9bfd8100e75aba0050efa8dc
        Author: vezamm <149106775+vezamm@users.noreply.github.com>
        Date:   Tue Oct 22 18:49:53 2024 +0100
    
            Movement
    
            cinemachine
            new input system
            character controller
            the character is not grounded at all time it must be fixed
    
        commit 0fd30cc2159af8f503bed831e9e4c1b8d0e33df3
        Author: vezamm <zdsellami@gmail.com>
        Date:   Mon Oct 21 14:37:24 2024 +0100
    
            cine machin
    
            capsule and plane with cine machine working
    
    commit 3e7c14c6b9371abe39d848e0c1adbd42f5a897c4
    Author: Aziz <azizbouziri10@gmail.com>
    Date:   Wed Oct 30 14:35:21 2024 +0100
    
        TT

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -18,70 +18,72 @@ public class PlayerController : MonoBehaviour
     private float gravityvalue = -9.81f;
     [SerializeField]
     private float rotationspeed = 4f;
-    [SerializeField]
-    private bool Grounded;
-
 
     private CharacterController characterController;
     private Vector3 playerVelocity;
     private PlayerControle _playercontrole;
     private Transform cameraMaintransform;
+
     private void Awake()
     {
-        _playercontrole=new PlayerControle();
+        _playercontrole = new PlayerControle();
     }
+
     private void OnEnable()
     {
-
         _playercontrole.Enable();
     }
+
     private void OnDisable()
     {
-
         _playercontrole.Disable();
     }
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        characterController =gameObject.GetComponent<CharacterController>();
-        cameraMaintransform=Camera.main.transform;
+        characterController = gameObject.GetComponent<CharacterController>();
+        cameraMaintransform = Camera.main.transform;
     }
 
     void Update()
     {
-        Grounded = characterController.isGrounded;
-        if (Grounded && playerVelocity.y < 0)
+        bool isGrounded = characterController.isGrounded; // Use CharacterController's built-in method
+        Debug.Log($"IsGrounded: {isGrounded}");
+
+        if (isGrounded && playerVelocity.y < 0)
         {
-            playerVelocity.y = 0;
+            playerVelocity.y = 0; // Reset vertical velocity if grounded
         }
+
         Vector2 movement = _playercontrole.Land.Move.ReadValue<Vector2>();
-        Vector3 move = new Vector3(movement.x, 0,movement.y) ;
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
 
         move = cameraMaintransform.forward * move.z + cameraMaintransform.right * move.x;
         move.y = 0;
 
-        Debug.Log(move);
-
-        if (_playercontrole.Land.Jump.triggered && Grounded)
+        if (_playercontrole.Land.Jump.triggered && isGrounded) // Jump only if grounded
         {
-            playerVelocity.y=Mathf.Sqrt(jumpheight*-3.0f*gravityvalue);
+            playerVelocity.y = Mathf.Sqrt(jumpheight * -2.0f * gravityvalue); // Adjusted for simplicity
         }
 
-        playerVelocity.y += gravityvalue * Time.deltaTime;
-        characterController.Move(playerVelocity*Time.deltaTime);
+        playerVelocity.y += gravityvalue * Time.deltaTime; // Apply gravity to the player every frame
+        characterController.Move(playerVelocity * Time.deltaTime);
 
-        if ( _playercontrole.Land.Sprinting.IsPressed())
+        if (_playercontrole.Land.Sprinting.IsPressed())
         {
             characterController.Move(move * Time.deltaTime * playerSprintspeed);
         }
         else
-           characterController.Move(move*Time.deltaTime*Playerspeed);
-        if(movement!=Vector2.zero)
         {
-            float targetangle=Mathf.Atan2(movement.x,movement.y)*Mathf.Rad2Deg+cameraMaintransform.eulerAngles.y;
-            Quaternion rotation = Quaternion.Euler(0f,targetangle,0f);
-            transform.rotation=Quaternion.Lerp(transform.rotation,rotation,Time.deltaTime*rotationspeed);
+            characterController.Move(move * Time.deltaTime * Playerspeed);
         }
-        
+
+        if (movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMaintransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationspeed);
+        }
     }
 }
---
+++ Assets/vezam's folder/scripts/Helping Ability.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering.Universal;
+
+
+public class HelpingAbility : MonoBehaviour
+{
+        
+        public Camera secondaryCamera; // Assign the secondary camera for highlights
+        public PostProcessVolume grayscaleVolume;
+        private bool isEagleVisionActive = false;
+
+        void Update()
+        {
+            grayscaleVolume=gameObject.GetComponent<PostProcessVolume>();
+            // Check for ability activation input (for example, pressing "E")
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ToggleEagleVision();
+            }
+        }
+
+        void ToggleEagleVision()
+        {
+            isEagleVisionActive = !isEagleVisionActive;
+
+            // Enable/disable grayscale effect
+            grayscaleVolume.enabled = isEagleVisionActive;
+
+            // Enable/disable secondary camera for highlighted objects
+            secondaryCamera.gameObject.SetActive(isEagleVisionActive);
+            secondaryCamera.enabled = isEagleVisionActive;
+        }
+    }
